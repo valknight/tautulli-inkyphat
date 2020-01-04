296b41d5c6477cfa937bf2347ef3246962bcd851
Val Knight <val+git@valknight.xyz>
2020-01-04 21:16:37 +0000

    Add script to show graph on inkyphat

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from PIL import Image, ImageFont, ImageDraw
+from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
+from font_intuitive import Intuitive
+from inky import InkyPHAT, InkyWHAT
+from tautulli_graph import play_count_graph
+# config
+mounted_upside_down = True
+colour = "yellow"
+strips = 5
+
+# Setup objects
+inky_display = InkyPHAT(colour)
+play_count_graph()
+img = Image.open("images/play_count.png")
+gray = img.convert('L')
+img = gray.point(lambda x: 255 if x<128 else 0, '1')
+
+if mounted_upside_down:
+    img = img.transpose(Image.ROTATE_180)
+img = img.resize((inky_display.WIDTH, inky_display.HEIGHT))
+inky_display.set_image(img)
+inky_display.show()
