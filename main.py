83a85543016a51298ee2e1910464f54f67d4e669
Val Knight <val+git@valknight.xyz>
2020-01-05 12:29:24 +0000

    Add new title text

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 
 from PIL import Image, ImageFont, ImageDraw
-from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
 from font_intuitive import Intuitive
 from inky import InkyPHAT, InkyWHAT
 from tautulli_graph import play_count_graph
-from config import colour, mounted_upside_down
+from config import colour, mounted_upside_down, show_text, days_to_show
 
 # Setup objects
 inky_display = InkyPHAT(colour)
-play_count_graph()
+play_count_graph(days=days_to_show)
 img = Image.open("images/play_count.png")
-gray = img.convert('L')
-img = gray.point(lambda x: 255 if x<128 else 0, '1')
+img = img.convert('L')
+img = img.point(lambda x: 255 if x < 240 else 0, '1')
+img = img.resize((inky_display.WIDTH, inky_display.HEIGHT))
+
+if show_text:
+    # Put title text on Pi
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(Intuitive, 22)
+
+    message = "Plex Activity"
+    w, h = font.getsize(message)
+    x = (inky_display.WIDTH / 2) - (w / 2)
+    y = 0
+    draw.text((x, y), message, inky_display.BLACK, font)
 
 if mounted_upside_down:
     img = img.transpose(Image.ROTATE_180)
-img = img.resize((inky_display.WIDTH, inky_display.HEIGHT))
+img.save("images/shown_graph.png", "PNG")
 inky_display.set_image(img)
 inky_display.show()
